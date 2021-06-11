67b634256a1cb5ecdef20a447f8ba1419da1e9b7
yejin <yejin0216@gmail.com>
2021-06-11 22:07:46 +0900

    first commit :boom:

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new ESLintPlugin(
+      {
+        exclude:["node_modules"],
+        fix: true
+      }
+    )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
