d8a6e5656cfa1c1175d6dfaa417300496e9b5d83
yejin kim <yejin0216@gmail.com>
2021-12-22 21:57:56 +0900

    arrow function으로 코드 스타일 수정

--- src/index.js
+++ src/index.js
@@ -2,31 +2,30 @@
  * @description 메인화면의 메뉴 목록을 조회하는 함수이다.
  * @returns {Promise} 플레이그라운드 메뉴 목록
  */
-async function getPlayItems() {
-  const result = await fetch(
-    '../src/assets/jsons/playItems.json'
-  ).then((resp) => resp.json());
-  return result;
-}
+const getPlayItems = async () => {
+  return await fetch('./src/assets/jsons/playItems.json').then(resp =>
+    resp.json(),
+  );
+};
 
 /**
  * @description 메인화면의 메뉴 목록을 생성하는 함수이다.
  * @returns {undefined} 반환값 없음
  */
-async function makeMainMenu() {
+const makeMainMenu = async () => {
   const ol = document.createElement('ol');
-  const playItems = await getPlayItems().then((resp) => resp);
+  const playItems = await getPlayItems().then(resp => resp);
 
-  playItems.forEach((item) => {
+  playItems.forEach(item => {
     const li = document.createElement('li');
     let a = document.createElement('a');
     a.href = item.href;
     a.textContent = item.name;
-    li.appendChild(a)
+    li.appendChild(a);
     ol.appendChild(li);
   });
 
   document.querySelector('main').appendChild(ol);
-}
+};
 
 makeMainMenu();
